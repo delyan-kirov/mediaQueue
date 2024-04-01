2faa032f02c4187bc93582dfa6db07dde5cebb6c
delyan-kirov <delyan.k.kirov@gmail.com>
2024-04-01 17:04:30 +0300

    Refactoring code

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,10 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main (main) where
 
+import Control.Lens
 import Control.Monad (when)
 import Data.IORef (IORef, modifyIORef, newIORef, readIORef, writeIORef)
 import Raylib.Core (
@@ -13,67 +18,75 @@ import Raylib.Core (
   setExitKey,
   setTargetFPS,
  )
-import Raylib.Core.Shapes (drawRectangle, drawRectangleRec)
+import Raylib.Core.Shapes (drawRectangleRec)
 import Raylib.Core.Text (drawText)
-import Raylib.Types (Color (Color), KeyboardKey (KeyA, KeyB, KeyC, KeyD, KeyNull, KeyS, KeySpace, KeyW), Rectangle (Rectangle, rectangle'height, rectangle'width, rectangle'x, rectangle'y))
+import Raylib.Types (
+  Color (Color),
+  KeyboardKey (KeyA, KeyD, KeyDown, KeyLeft, KeyNull, KeyRight, KeyS, KeySpace, KeyUp, KeyW),
+  Rectangle (Rectangle, rectangle'height, rectangle'width, rectangle'x, rectangle'y),
+ )
 import Raylib.Util (whileWindowOpen0)
 import Raylib.Util.Colors qualified as Colors
 
+$(makeLenses ''Rectangle)
+makeLensesFor
+  [ ("rectangleX", "_rectangleX")
+  , ("rectangleY", "_rectangleY")
+  , ("rectangleHeight", "_rectangleHeight")
+  , ("rectangleWidth", "_rectangleWidth")
+  ]
+  ''Rectangle
+
+data GameState = GameState
+  { _gameMode :: IORef Int
+  , _entity :: IORef Rectangle
+  }
+
+$(makeLenses ''GameState)
+
+initGameState :: IO GameState
+initGameState = do
+  gameModeRef <- newIORef 1
+  rectangleRef <- newIORef $ Rectangle 30.0 30.0 50.0 50.0
+  return $
+    GameState
+      { _gameMode = gameModeRef
+      , _entity = rectangleRef
+      }
+
 moveDirection :: Rectangle -> IO Rectangle
-moveDirection rect = do
+moveDirection r = do
   getPressedKey <- getKeyPressed
   case getPressedKey of
-    KeyA ->
-      return $
-        Rectangle
-          (rectangle'x rect - 50.0)
-          (rectangle'y rect)
-          (rectangle'height rect)
-          (rectangle'width rect)
-    KeyW ->
-      return $
-        Rectangle
-          (rectangle'x rect)
-          (rectangle'y rect - 50.0)
-          (rectangle'height rect)
-          (rectangle'width rect)
-    KeyD ->
-      return $
-        Rectangle
-          (rectangle'x rect + 50.0)
-          (rectangle'y rect)
-          (rectangle'height rect)
-          (rectangle'width rect)
-    KeyS ->
-      return $
-        Rectangle
-          (rectangle'x rect)
-          (rectangle'y rect + 50.0)
-          (rectangle'height rect)
-          (rectangle'width rect)
-    _ -> return rect
+    x | x `elem` [KeyA, KeyLeft] -> return $ r{rectangle'x = r.rectangle'x - 50.0}
+    x | x `elem` [KeyW, KeyUp] -> return $ r{rectangle'y = r.rectangle'y - 50.0}
+    x | x `elem` [KeyD, KeyRight] -> return $ r{rectangle'x = r.rectangle'x + 50.0}
+    x | x `elem` [KeyS, KeyDown] -> return $ r{rectangle'y = r.rectangle'y + 50.0}
+    _ -> return r
 
 switchMode :: IORef Int -> IO ()
-switchMode gameMode = do
+switchMode gameMode' = do
   shouldGoNext <- isKeyPressed KeySpace
-  when shouldGoNext $ modifyIORef gameMode (\x -> (x + 1) `mod` 3)
+  when shouldGoNext $ modifyIORef gameMode' (\x -> (x + 1) `mod` 3)
 
-gameloop :: IORef Int -> IORef Rectangle -> IO ()
-gameloop gameMode rect1 = do
+gameloop :: GameState -> IO ()
+gameloop gameState = do
   beginDrawing
 
-  switchMode gameMode
-  currMode <- readIORef gameMode
+  switchMode (gameState ^. gameMode)
+  currMode <- readIORef (gameState ^. gameMode)
   case currMode of
     1 -> do
       clearBackground $ Color 100 100 100 1
       drawText "POG" 50 50 40 Colors.rayWhite
     2 -> do
-      oldRectangle <- readIORef rect1
+      -- update position
+      oldRectangle <- readIORef (gameState ^. entity)
       newRectangle <- moveDirection oldRectangle
-      writeIORef rect1 newRectangle
-      clearBackground $ Color 100 100 100 1
-      drawRectangleRec newRectangle Colors.rayWhite
+      writeIORef (gameState ^. entity) newRectangle
+      -- draw
+      clearBackground $ Color 120 180 240 1
+      drawRectangleRec newRectangle Colors.pink
     _ -> do
       clearBackground $ Color 100 100 200 1
       drawText "Basic raylib window" 50 50 40 Colors.rayWhite
@@ -82,12 +95,11 @@ gameloop gameMode rect1 = do
 main :: IO ()
 main = do
   raylib <- initWindow 600 450 "TEST RAYLIB FROM HASKELL"
-  gameModeRef <- newIORef 1
-  rectangleRef <- newIORef $ Rectangle 30.0 30.0 50.0 50.0
   setTargetFPS 60
   setExitKey KeyNull
+  gameState <- initGameState
 
   whileWindowOpen0 $ do
-    gameloop gameModeRef rectangleRef
+    gameloop gameState
 
   closeWindow raylib
